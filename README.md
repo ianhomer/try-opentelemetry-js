b30d65dc8c5f1530914a76587f8ca6b2524a533a
Ian Homer <code@ianhomer.com>
2024-04-28 20:09:34 +0100

    ✨ spin up a collector to see traces

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # Try OpenTelemetry JS
 
     pnpm i
+
+Start up a collector
+
+    docker run \
+      -p 127.0.0.1:4317:4317 \
+      -p 127.0.0.1:55679:55679 \
+      otel/opentelemetry-collector:latest \
+      2>&1 | tee collector-output.txt
+
+Start the express application
+
     pnpm start
 
 And then hit API
 
     curl http://localhost:3000
 
-and see tracing written to console.
+and see tracing written to console or visit
+<http://localhost:55679/debug/tracez> to see traces.
--- src/tracing.js
+++ src/tracing.js
@@ -1,23 +1,65 @@
 // tracing.js
 
-'use strict'
+"use strict";
 
-const process = require('process');
-const opentelemetry = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const process = require("process");
+const uuid = require("uuid");
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  WebTracerProvider,
+  BatchSpanProcessor,
+} = require("@opentelemetry/sdk-trace-web");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
+const { Resource } = require("@opentelemetry/resources");
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
 
 // configure the SDK to export telemetry data to the console
 // enable all auto-instrumentations from the meta package
-const traceExporter = new ConsoleSpanExporter();
+//
+//
+const customHeaders = {
+  foo: "foo-value",
+};
+
+const collectorExporter = {
+  url: "http://127.0.0.1:4317/v1/traces",
+  headers: customHeaders,
+  concurrencyLimit: 10,
+};
+const exporter = new OTLPTraceExporter(
+  collectorExporter,
+);
+
+const OTEL_SERVICE_RESOURCE = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "otel-express-node",
+  [SemanticResourceAttributes.SERVICE_VERSION]: "1.0.0",
+  [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: uuid.v4(),
+});
+
+const provider = new WebTracerProvider({ resource: OTEL_SERVICE_RESOURCE });
+provider.addSpanProcessor(
+  new BatchSpanProcessor(exporter, {
+    // The maximum queue size. After the size is reached spans are dropped.
+    maxQueueSize: 1000,
+    // The interval between two consecutive exports
+    scheduledDelayMillis: 1000,
+  }),
+);
+provider.register();
+
 const sdk = new opentelemetry.NodeSDK({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'my-service',
+    [SemanticResourceAttributes.SERVICE_NAME]: "my-service",
   }),
-  traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()]
+  exporter,
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
 // initialize the SDK and register with the OpenTelemetry API
@@ -25,9 +67,10 @@ const sdk = new opentelemetry.NodeSDK({
 sdk.start();
 
 // gracefully shut down the SDK on process exit
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('Tracing terminated'))
-    .catch((error) => console.log('Error terminating tracing', error))
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => console.log("Tracing terminated"))
+    .catch((error) => console.log("Error terminating tracing", error))
     .finally(() => process.exit(0));
 });
